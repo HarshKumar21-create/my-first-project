3cbf3e79212bfa115a52c3af97f044e739bf9ee6
Harsh Kumar <hk0107342@gmail.com>
2026-06-23 19:16:07 +0530

    Added login.js

---
+++ login.js
@@ -0,0 +1,29 @@
+let password= document.querySelector("#password");
+let showBtn= document.querySelector("#shw-button");
+let loginBtn = document.querySelector("#login-btn");
+showBtn.addEventListener("click",
+    function(){
+    if(password.type === "password"){
+    password.type = "text";
+    showBtn.innerText = "Hide";
+}
+else{
+   password.type = "password";
+   showBtn.innerText = "Show";
+}
+});
+
+loginBtn.addEventListener("click",
+    function(){
+        if(password.value === ""){
+            alert("Please enter password");
+        }
+        else if(password.value.length < 8){
+        alert("Password must be at least 8 characters");
+     }
+        else{
+            alert("Login Successful");
+        }
+     });
+
+
